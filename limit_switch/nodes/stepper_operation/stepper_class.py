2a64e44060f5b47dd2d606e1c6f46950513054bf
arunavanag591 <arunavoster@gmail.com>
2020-02-09 18:05:23 -0800

    continuous horizontal and looped vertical implemented

--- limit_switch/nodes/stepper_operation/stepper_class.py
+++ limit_switch/nodes/stepper_operation/stepper_class.py
@@ -19,7 +19,7 @@ import time
 import sys
 
 
-def get_stepper_motor(serial):
+def get_stepper_motor0(serial):
     try:
         ch = Stepper()
     except RuntimeError as e:
@@ -83,16 +83,83 @@ def get_stepper_motor(serial):
     return ch 
 
 
+def get_stepper_motor1(serial):
+    try:
+        ch1 = Stepper()
+    except RuntimeError as e:
+        print("Runtime Exception %s" % e.details)
+        print("Press Enter to Exit...\n")
+        readin = sys.stdin.read(1)
+        exit(1)
+
+    def StepperAttached(self):
+        try:
+            attached = self
+            print("\nAttach Event Detected (Information Below)")
+            print("===========================================")
+            print("Library Version: %s" % attached.getLibraryVersion())
+            print("Serial Number: %d" % attached.getDeviceSerialNumber())
+            print("Channel: %d" % attached.getChannel())
+            print("Channel Class: %s" % attached.getChannelClass())
+            print("Channel Name: %s" % attached.getChannelName())
+            print("Device ID: %d" % attached.getDeviceID())
+            print("Device Version: %d" % attached.getDeviceVersion())
+            print("Device Name: %s" % attached.getDeviceName())
+            print("Device Class: %d" % attached.getDeviceClass())
+            print("\n")
+
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.details))
+            print("Press Enter to Exit...\n")
+            readin = sys.stdin.read(1)
+            exit(1)   
+        
+    def StepperDetached(self):
+        detached = self
+        try:
+            print("\nDetach event on Port %d Channel %d" % (detached.getHubPort(), detached.getChannel()))
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.details))
+            print("Press Enter to Exit...\n")
+            readin = sys.stdin.read(1)
+            exit(1)   
+
+    def ErrorEvent(self, eCode, description):
+        print("Error %i : %s" % (eCode, description))
+
+    def PositionChangeHandler(self, position):
+        print("Position: %f" % position)
+
+    try:
+        ch1.setDeviceSerialNumber(serial)
+        ch1.setOnAttachHandler(StepperAttached)
+        ch1.setOnDetachHandler(StepperDetached)
+        ch1.setOnErrorHandler(ErrorEvent)
+
+        print("Waiting for the Phidget Stepper Object to be attached...")
+        ch1.openWaitForAttachment(5000)
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Press Enter to Exit...\n")
+        readin = sys.stdin.read(1)
+        exit(1)
+        
+    return ch1 
+
 
 class Stepper_Control:
-  def __init__(self, ch):
+  def __init__(self, ch, ch1):
     
     # Set up the Stepper Motor
     self.ch0 = ch
-    
+    #self.ch0.setDeviceSerialNumber(505962)
+    self.ch1 = ch1
+    #self.ch0.setDeviceSerialNumber(522470)
+
     rospy.Subscriber("mag_switch", Int8MultiArray, self.limit_switch)  
 
     self.ch0_position = 0
+    self.ch1_position = 0
     self.limit_switch_0 = 0
     self.limit_switch_1 = 0
     self.limit_switch_2 = 0
@@ -111,7 +178,7 @@ class Stepper_Control:
     self.limit_switch_3 = data.data[3]
         
 
-  def move_motor_safely_towards_0(self, desired_position):
+  def move_motor_safely_towards_0(self, desired_position): #right
     increment = 10
     while self.limit_switch_0 == 0 and self.ch0_position < desired_position:
       self.ch0.setTargetPosition(self.ch0_position + increment)
@@ -119,41 +186,61 @@ class Stepper_Control:
       rospy.sleep(0.05)
       
 
-  def move_motor_safely_towards_1(self, desired_position):
-  
+  def move_motor_safely_towards_1(self, desired_position):  #left
     increment = -10
     while self.limit_switch_1 == 0 and self.ch0_position > desired_position:
       self.ch0.setTargetPosition(self.ch0_position + increment)
       self.ch0_position += increment
       rospy.sleep(0.05)
 
-  def move_motor_safely_towards_2(self, desired_position):
+  def move_motor_safely_towards_2(self, desired_position): #up
     print (desired_position)
     increment = 100
-    while self.limit_switch_2 == 0 and self.ch0_position < desired_position:
-      self.ch0.setTargetPosition(self.ch0_position + increment)
-      self.ch0_position += increment
+    while self.limit_switch_2 == 0 and self.ch1_position < desired_position:
+      self.ch1.setTargetPosition(self.ch1_position + increment)
+      self.ch1_position += increment
       rospy.sleep(0.05)
 
-  def move_motor_safely_towards_3(self, desired_position):    
+  def move_motor_safely_towards_3(self, desired_position):  #down
     increment = -100
-    while self.limit_switch_3 == 0 and self.ch0_position > desired_position:
-      self.ch0.setTargetPosition(self.ch0_position + increment)
-      self.ch0_position += increment
+    while self.limit_switch_3 == 0 and self.ch1_position > desired_position:
+      self.ch1.setTargetPosition(self.ch1_position + increment)
+      self.ch1_position += increment
       rospy.sleep(0.05)
 
+  def move_horizontal(self): #horizontalmovement
+    increment1 = 500
+    increment2 = -500 
 
+    while self.limit_switch_0 == 0:
+      self.move_motor_safely_towards_0(self.ch0_position+increment1)
+      rospy.sleep(0.5)
 
-  def move_motor_towards_1(self):
-  
+    while self.limit_switch_1 == 0:
+      self.move_motor_safely_towards_1(self.ch0_position+increment2)
+      rospy.sleep(0.5)
+      
+
+  def move_motor(self):
+    
     try:
-        increment = 500
         
-        if self.limit_switch_2 == 0:
-          print (self.limit_switch_2)
-          self.move_motor_safely_towards_2(self.ch0_position+increment)
-          rospy.sleep(2)
-        
+        increment1 = 5000
+        increment2 = -5000 
+               
+        for x in range(0,5):
+          if self.limit_switch_2 == 0:
+            self.move_motor_safely_towards_2(self.ch1_position+increment1)
+            rospy.sleep(0.5)
+            self.move_horizontal()
+            rospy.sleep(0.5)
+
+          elif self.limit_switch_3 == 0:
+            self.move_motor_safely_towards_3(self.ch1_position+increment2)
+            rospy.sleep(0.5)
+            self.move_horizontal()
+            rospy.sleep(0.5)
+
         print ("loopexited")
           
 
--- limit_switch/nodes/stepper_operation/stepper_main.py
+++ limit_switch/nodes/stepper_operation/stepper_main.py
@@ -6,19 +6,17 @@ import stepper_class
 
 if __name__ == '__main__':
   try:
-    ch0 = stepper_class.get_stepper_motor(522470)
+    #Horizontal Motor - x
+    ch0 = stepper_class.get_stepper_motor0(505962)
     ch0.setEngaged(True)
-    #ch0.setDeviceSerialNumber(505962)
-    
-
-    #ch1 = stepper_class.get_stepper_motor()
-    #ch0.setDeviceSerialNumber(522470)
-    #ch1.setEngaged(True)
+    #Vertical Motor - y
+    ch1 = stepper_class.get_stepper_motor1(522470)
+    ch1.setEngaged(True)
 
     rospy.init_node('stepper_motor', anonymous=True)
 
-    stepper = stepper_class.Stepper_Control(ch0)
-    stepper.move_motor_towards_1()
+    stepper = stepper_class.Stepper_Control(ch0, ch1)
+    stepper.move_motor()
 
   except rospy.ROSInterruptException:
     pass
