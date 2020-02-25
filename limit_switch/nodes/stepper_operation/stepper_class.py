63365b3515d3a88c42806137d620065be0e57621
buzz <austinlopez@nevada.unr.edu>
2020-02-25 11:14:33 -0800

    added motor publisher and time stamp info

--- limit_switch/nodes/stepper_operation/stepper_class.py
+++ limit_switch/nodes/stepper_operation/stepper_class.py
@@ -10,6 +10,8 @@ import atexit
 import roslib, rospy
 import serial
 from std_msgs.msg import Int8MultiArray
+from std_msgs.msg import Int64MultiArray
+# from std_msgs.msg import Float32MultiArray
 
 from Phidget22.PhidgetException import *
 from Phidget22.Phidget import *
@@ -155,8 +157,10 @@ class Stepper_Control:
     #self.ch0.setDeviceSerialNumber(505962)
     self.ch1 = ch1
     #self.ch0.setDeviceSerialNumber(522470)
+    # self.stepper_motor = get_stepper_motor0(505962)
 
-    rospy.Subscriber("mag_switch", Int8MultiArray, self.limit_switch)  
+    rospy.Subscriber("mag_switch", Int8MultiArray, self.limit_switch)
+    self.publisher = rospy.Publisher('motopub', Int64MultiArray, queue_size=10)  
 
     self.ch0_position = 0
     self.ch1_position = 0
@@ -168,7 +172,7 @@ class Stepper_Control:
 
     atexit.register(self.shutdown_stepper)
 
-    
+    self.publish_motor_data()
 
 ################################################################################
 
@@ -178,6 +182,11 @@ class Stepper_Control:
     self.limit_switch_2 = data.data[2]
     self.limit_switch_3 = data.data[3]
         
+  def publish_motor_data(self):
+    print(self.ch0_position, self.ch1_position)
+    msg = Int64MultiArray()
+    msg.data = [int(self.ch0_position), int(self.ch1_position)]
+    self.publisher.publish(msg)
 
   def move_motor_safely_towards_0(self, desired_position): #right
     increment = 10
@@ -193,6 +202,7 @@ class Stepper_Control:
       self.ch0.setTargetPosition(self.ch0_position + increment)
       self.ch0_position += increment
       rospy.sleep(0.05)
+    
 
   def move_motor_safely_towards_2(self, desired_position): #up
     print (desired_position)
@@ -208,22 +218,32 @@ class Stepper_Control:
       self.ch1.setTargetPosition(self.ch1_position + increment)
       self.ch1_position += increment
       rospy.sleep(0.05)
+     
 
+        
   def move_horizontal(self): #horizontalmovement
     #horizontal motor step size
-    increment1 = 500
-    increment2 = -500 
+    increment1 = 1000
+    increment2 = -1000 
+    
 
     while self.limit_switch_0 == 0:
       self.move_motor_safely_towards_0(self.ch0_position+increment1)
-      rospy.sleep(0.5)
+      rospy.sleep(30)# change this timing to change the horizontal step gap duration
+      self.publish_motor_data()
+      # MES.data = rospy.Time.now().secs
+      # self.publisher.publish(MES) 
 
     while self.limit_switch_1 == 0:
       self.move_motor_safely_towards_1(self.ch0_position+increment2)
-      rospy.sleep(0.5)
-      
+      rospy.sleep(30)# change this timing to change the horizontal step gap duration
+      # MES.data = 
+      # self.publisher.publish(MES)
+      self.publish_motor_data()
+
 
   def move_motor(self):
+    # showtime()
     try:
         #vertical motor step size
         increment1 = 50000
@@ -232,15 +252,15 @@ class Stepper_Control:
         for x in range(0,1):  #number of vertical cycles
           while self.limit_switch_2 == 0:
             self.move_motor_safely_towards_2(self.ch1_position+increment1)
-            rospy.sleep(0.5) # change this timing to change the gap between vertical and horizontal
+            rospy.sleep(0.5) # change this timing to change moving down
             self.move_horizontal()
-            rospy.sleep(0.5) # change this timing to change the horizontal step gap duration
-
+            rospy.sleep(.5) 
           while self.limit_switch_3 == 0:
             self.move_motor_safely_towards_3(self.ch1_position+increment2)
             rospy.sleep(0.5)
             self.move_horizontal()
             rospy.sleep(0.5)
+
 
         print ("loopexited")
           
@@ -251,7 +271,34 @@ class Stepper_Control:
         print("")
         print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
 
+  
+
   def shutdown_stepper(self):
     self.ch0.setEngaged(0)
     self.ch0.close()    
     print("Stopped Stepper device")
+
+  # def showtime(self):
+
+    # rate = rospy.Rate(40) # Hz
+    # while not rospy.is_shutdown():
+    #     #if self.state is not None:
+    #     #    msg = Bool()
+    #     #    msg.data=self.state
+    #     #    self.publisher.publish(msg)
+        
+    #     msg  = Int8MultiArray()
+    #     pos1 = int(self.ch0.getPosition())
+    #     pos2 = int(self.ch1.getPosition())
+    #     print("except11",[pos1,pos2, rospy.Time.now().secs])
+        
+    #   # MES.data=[pos1,pos2]
+    #   # self.publisher.publish(MES)
+    #     print([pos1,pos2,rospy.Time.now().secs])
+    #     msg.data = [pos1,pos2,rospy.Time.now().secs] #,self.state[4],self.state[5]]
+    #     self.publisher.publish(msg)
+
+    #     rate.sleep()
+
+
+
